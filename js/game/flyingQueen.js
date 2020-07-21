234d1b88c133a1a4358b282ecbbc6a17841a519f
brianjboucher <brianb823@gmail.com>
2020-07-20 19:48:08 -0500

    Clicking/touching controls height of flying queen in flying game. Replaced queen sprite.

--- js/game/flyingQueen.js
+++ js/game/flyingQueen.js
@@ -2,21 +2,50 @@ class FlyingQueen {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.speed = 5;
-        this.sprite = new Sprite(tr(vec2(this.x * pixelSize, this.y * pixelSize), vec2(pixelSize / 3, pixelSize / 3)), new ImageObject("images/placeholder_antQueen.png", vec2(96, 96)));
+        this.horizontalSpeed = 2;
+        this.verticalSpeed = 3;
+        //this.sprite = new Sprite(tr(vec2(this.x * pixelSize, this.y * pixelSize), vec2(pixelSize / 3, pixelSize / 3)), new ImageObject("images/placeholder_antQueen.png", vec2(96, 96)));
+        this.sprite = new Sprite(tr(vec2(this.x * pixelSize, this.y * pixelSize), vec2(pixelSize / 3, pixelSize / 3)), new ImageObject("Visual Assets/8bit-Queen-right.png", vec2(96, 96)));
+        this.flyingMomentum = 0;
+        this.fallingMomentum = 0;
+
+        this.movementStates = {
+            FLYING: "flying",
+            FALLING: "falling"
+        }
+        this.movementState = this.movementStates.FALLING;
 
         flightGameUI.push(this);
     }
 
     event() {
+
         if (isTouched) {
-            this.sprite.transform.position.x = touchPos[0].x - canvas.getBoundingClientRect().left;
-            this.sprite.transform.position.y = touchPos[0].y - canvas.getBoundingClientRect().top;
+            this.movementState = this.movementStates.FLYING;
+        } else {
+            this.movementState = this.movementStates.FALLING;
         }
+
     }
 
     update() {
-        this.sprite.transform.position.x += this.speed;
+
+        switch (this.movementState) {
+            case 'flying':
+                if (this.sprite.transform.position.y > 0) {
+                    this.sprite.transform.position.y -= this.verticalSpeed * 3;
+                }
+                break;
+            case 'falling':
+                if (this.sprite.transform.position.y < gameHeight) {
+                    this.sprite.transform.position.y += this.verticalSpeed;
+                }
+
+                break;
+        }
+
+        this.sprite.transform.position.x += this.horizontalSpeed;
+        //this.x += this.horizontalSpeed;
     }
 
     draw() {
