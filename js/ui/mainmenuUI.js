c6d498f9158996a0c07e568e13fbca0112cbf5f5
stebssbets <stebssbets@yahoo.com>
2020-07-31 14:07:53 +0700

    added a button for Fly Defense Game in the main menu

--- js/ui/mainmenuUI.js
+++ js/ui/mainmenuUI.js
@@ -8,24 +8,48 @@ var mainMenuFontSize = 32;
 function setupMainMenuUI()
 {
     menuButtons = [];
+    // TextButton           constructor(transform, label, button, tooltip)
+    // Transform            function tr(pos, sc, rot, orig) { return new Transform(pos, sc, rot, orig); }
+    // Vec2                 constructor(x, y)
+    // Label                constructor(transform, text, font, textColor, align, tooltip)
+    // Button               constructor(transform, btnColor, selectColor, hoverColor, disabledColor)
+
     leafcuttingBtn = new TextButton(tr(vec2(), btnSize),
-        new Label(tr(), "  LEAFCUTTING GAME",
-        mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
-        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), "");
+        new Label(tr(), "  LEAFCUTTING GAME", mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
+        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), 
+        "");
     menuButtons.push(leafcuttingBtn);
+
     colonyBtn = new TextButton(tr(vec2(), btnSize),
-        new Label(tr(), "  COLONY GAME",
-        mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
-        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), "");
+        new Label(tr(), "  COLONY GAME", mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
+        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), 
+        "");
     menuButtons.push(colonyBtn);
+
     flightBtn = new TextButton(tr(vec2(), btnSize),
-        new Label(tr(), "  FLIGHT GAME",
-        mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
-        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), "");
+        new Label(tr(), "  FLIGHT GAME", mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
+        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), 
+        "");
     menuButtons.push(flightBtn);
 
-    mainMenuUI.push(new FlexGroup(tr(vec2(50, 25), vec2(gameWidth, gameHeight)),
-        new SubState(tr(), menuButtons),false, vec2(50, 20), vec2(1, 3), true));
+    defenseBtn = new TextButton(tr(vec2(), btnSize),
+        new Label(tr(), "  FLY DEFENSE GAME", mainMenuFontSize.toString() + "px " + uiContext.fontFamily, "black", 0),
+        new Button(tr(), "#ffffffff", "#99ff99ff", "#bbffbbff"), 
+        "");
+    menuButtons.push(defenseBtn);
+    
+    console.log('menuButtons: ' + menuButtons);
+
+// FlexGroup constructor(transform, subState, isAxisVertical, gridSpace, gridSize, scaleFlex)
+//function tr(pos, sc, rot, orig) { return new Transform(pos, sc, rot, orig); }
+//SubState constructor(transform, uiObjects)
+    mainMenuUI.push(
+        new FlexGroup(tr(vec2(50, 25), vec2(gameWidth, gameHeight)),
+        new SubState(tr(), menuButtons),
+        false, 
+        vec2(50, 20), 
+        vec2(1, 4), 
+        true));
 }
 
 function mainMenuUICustomDraw(deltaTime)
@@ -47,6 +71,11 @@ function mainMenuUICustomEvents(deltaTime)
     {
         ui.stateIndex = FLIGHTGAMEUI;
         flightBtn.button.resetOutput();
+    }
+    else if(defenseBtn.button.output == UIOUTPUT_SELECT)
+    {
+        ui.stateIndex = DEFENSEGAMEUI;
+        defenseBtn.button.resetOutput();
     }
     else if(colonyBtn.button.output == UIOUTPUT_SELECT)
     {
