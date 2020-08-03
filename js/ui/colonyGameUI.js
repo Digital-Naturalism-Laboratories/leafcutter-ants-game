c07e5b5b4f9710bb305e79b14a6c7680b30716da
brianjboucher <brianb823@gmail.com>
2020-08-02 21:43:23 -0500

    Colony Game - Queen moves to last clicked or touched position, creating tunnels along the way

--- js/ui/colonyGameUI.js
+++ js/ui/colonyGameUI.js
@@ -12,21 +12,21 @@ var worker_right = document.createElement('img');
 
 var colonyTiles = [];
 
-var colonyGrid = [  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 3, 2, 2, 2, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 2, 2, 2, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 3, 2, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+var colonyGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 3, 2, 2, 2, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 2, 2, 2, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 3, 2, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
 
 const COLONY_WALL = 0;
@@ -43,116 +43,150 @@ var population = 0;
 
 function setupColonyGameUI() {
 
-    loadImages();
-    queen = new Queen(400 * pixelSize, 220 * pixelSize);
+  loadImages();
+  queen = new Queen(400 * pixelSize, 220 * pixelSize);
 }
 
 function colonyGameUICustomDraw(deltaTime) {
 
-    renderer.drawImage(groundBG, 0, 0, gameWidth, gameHeight - (120 * pixelSize));
+  renderer.drawImage(groundBG, 0, 0, gameWidth, gameHeight - (120 * pixelSize));
 
-    drawColonyTiles();
-    renderer.drawImage(fungusNest, gameWidth * 0.75, gameHeight * 0.4, gameWidth / 5, gameHeight / 5);
+  digTunnel();
+  drawColonyTiles();
+  renderer.drawImage(fungusNest, gameWidth * 0.75, gameHeight * 0.4, gameWidth / 5, gameHeight / 5);
 
-    renderer.drawImage(worker_right, (colonyTiles[COLONY_TUNNEL_VERT].width * 10) * pixelSize, (colonyTiles[COLONY_TUNNEL_VERT].height * 7 + 10) * pixelSize, (worker_right.width / 2) * pixelSize, (worker_right.height / 2) * pixelSize);
+  renderer.drawImage(worker_right, (colonyTiles[COLONY_TUNNEL_VERT].width * 10) * pixelSize, (colonyTiles[COLONY_TUNNEL_VERT].height * 7 + 10) * pixelSize, (worker_right.width / 2) * pixelSize, (worker_right.height / 2) * pixelSize);
 
-    renderer.fillStyle = 'black';
-    renderer.fillRect(0, gameHeight - (120 * pixelSize), gameWidth, 120 * pixelSize);
+  renderer.fillStyle = 'black';
+  renderer.fillRect(0, gameHeight - (120 * pixelSize), gameWidth, 120 * pixelSize);
 
-    renderer.fillStyle = "white";
-    renderer.font = (24 * pixelSize).toString() + "px Arial";
+  renderer.fillStyle = "white";
+  renderer.font = (24 * pixelSize).toString() + "px Arial";
 
-    renderer.fillText("Fungus: " + fungusMass + "g", 50 * pixelSize, gameHeight - (85 * pixelSize));
-    renderer.fillText("Incoming Leaves: " + incomingLeaves + "g", 50 * pixelSize, gameHeight - (50 * pixelSize));
-    renderer.fillText("Workers: " + workerCount, 50 * pixelSize, gameHeight - (15 * pixelSize));
+  renderer.fillText("Fungus: " + fungusMass + "g", 50 * pixelSize, gameHeight - (85 * pixelSize));
+  renderer.fillText("Incoming Leaves: " + incomingLeaves + "g", 50 * pixelSize, gameHeight - (50 * pixelSize));
+  renderer.fillText("Workers: " + workerCount, 50 * pixelSize, gameHeight - (15 * pixelSize));
 
-    renderer.fillText("Population: " + population, gameWidth - (300 * pixelSize), gameHeight - (85 * pixelSize));
-    renderer.fillText("Eggs: " + eggCount, gameWidth - (300 * pixelSize), gameHeight - (50 * pixelSize));
-    renderer.fillText("Larvae: " + larvaeCount, gameWidth - (300 * pixelSize), gameHeight - (15 * pixelSize));
+  renderer.fillText("Population: " + population, gameWidth - (300 * pixelSize), gameHeight - (85 * pixelSize));
+  renderer.fillText("Eggs: " + eggCount, gameWidth - (300 * pixelSize), gameHeight - (50 * pixelSize));
+  renderer.fillText("Larvae: " + larvaeCount, gameWidth - (300 * pixelSize), gameHeight - (15 * pixelSize));
 
 }
 
 function colonyGameUICustomEvents(deltaTime) {
-    if (isTouched) {
-        if (getDistBtwVec2(vec2((colonyTiles[COLONY_TUNNEL_VERT].width * 10) * pixelSize, (colonyTiles[COLONY_TUNNEL_VERT].height * 7 + 10) * pixelSize), vec2(touchPos[0].x - canvas.getBoundingClientRect().left, touchPos[0].y - canvas.getBoundingClientRect().top)) < 40 * pixelSize) {
-            ui.stateIndex = LEAFCUTTINGUI;
-        }
+  if (isTouched) {
+    if (getDistBtwVec2(vec2((colonyTiles[COLONY_TUNNEL_VERT].width * 10) * pixelSize, (colonyTiles[COLONY_TUNNEL_VERT].height * 7 + 10) * pixelSize), vec2(touchPos[0].x - canvas.getBoundingClientRect().left, touchPos[0].y - canvas.getBoundingClientRect().top)) < 40 * pixelSize) {
+      ui.stateIndex = LEAFCUTTINGUI;
     }
+  }
 
 }
 
 function colonyTileToIndex(tileCol, tileRow) {
-    return (tileCol + COLONY_COLS * tileRow);
+  return (tileCol + COLONY_COLS * tileRow);
 }
 
 function getColonyTileAtPixelCoord(pixelX, pixelY) {
-    var tileCol = (pixelX / COLONY_W) * pixelSize;
-    var tileRow = (pixelY / COLONY_H) * pixelSize;
+  var tileCol = (pixelX / COLONY_W) * pixelSize;
+  var tileRow = (pixelY / COLONY_H) * pixelSize;
 
-    tileCol = Math.floor(tileCol);
-    tileRow = Math.floor(tileRow);
+  tileCol = Math.floor(tileCol);
+  tileRow = Math.floor(tileRow);
 
-    if (tileCol < 0 || tileCol >= COLONY_COLS ||
-        tileRow < 0 || tileRow >= COLONY_ROWS) {
-        return COLONY_WALL;
-    }
+  if (tileCol < 0 || tileCol >= COLONY_COLS ||
+    tileRow < 0 || tileRow >= COLONY_ROWS) {
+    return COLONY_WALL;
+  }
 
-    var colonyIndex = colonyTileToIndex(tileCol, tileRow);
-    return colonyGrid[colonyIndex];
+  var colonyIndex = colonyTileToIndex(tileCol, tileRow);
+  return colonyGrid[colonyIndex];
+}
+
+function colAtXCoord(pixelX) {
+  return Math.floor(pixelX / COLONY_W);
+}
+
+function rowAtYCoord(pixelY) {
+  return Math.floor(pixelY / COLONY_H);
+}
+
+function mapTileToIndex(tileCol, tileRow) {
+  return (tileCol + COLONY_COLS * tileRow);
 }
 
 function drawColonyTiles() {
-    var colonyIndex = 0;
-    var colonyLeftEdgeX = 0;
-    var colonyTopEdgeY = 0;
+  var colonyIndex = 0;
+  var colonyLeftEdgeX = 0;
+  var colonyTopEdgeY = 0;
 
-    for (var eachRow = 0; eachRow < COLONY_ROWS; eachRow++) {
+  for (var eachRow = 0; eachRow < COLONY_ROWS; eachRow++) {
 
-        colonyLeftEdgeX = 0;
+    colonyLeftEdgeX = 0;
 
-        for (var eachCol = 0; eachCol < COLONY_COLS; eachCol++) {
+    for (var eachCol = 0; eachCol < COLONY_COLS; eachCol++) {
 
-            var colonyTypeHere = colonyGrid[colonyIndex];
+      var colonyTypeHere = colonyGrid[colonyIndex];
 
-            if (colonyGrid[colonyIndex] != COLONY_WALL) {
-                renderer.drawImage(colonyTiles[colonyTypeHere], colonyLeftEdgeX, colonyTopEdgeY, COLONY_W * pixelSize, COLONY_H * pixelSize);
-            }
+      if (colonyGrid[colonyIndex] != COLONY_WALL) {
+        renderer.drawImage(colonyTiles[colonyTypeHere], colonyLeftEdgeX, colonyTopEdgeY, COLONY_W * pixelSize, COLONY_H * pixelSize);
+      }
 
-            colonyIndex++;
-            colonyLeftEdgeX += COLONY_W * pixelSize;
-
-        }
-
-        colonyTopEdgeY += COLONY_H * pixelSize;
+      colonyIndex++;
+      colonyLeftEdgeX += COLONY_W * pixelSize;
 
     }
+
+    colonyTopEdgeY += COLONY_H * pixelSize;
+
+  }
+}
+
+function digTunnel() {
+  colonyGrid[mapTileToIndex(colAtXCoord(queen.x / pixelSize), rowAtYCoord(queen.y / pixelSize))] = COLONY_TUNNEL_4WAY;
 }
 
 function beginLoadingImage(imgVar, fileName) {
-  imgVar.src="Visual Assets/"+fileName;
+  imgVar.src = "Visual Assets/" + fileName;
 }
 
 function loadImageForTrackCode(tunnelCode, fileName) {
-    colonyTiles[tunnelCode] = document.createElement("img");
-  beginLoadingImage(colonyTiles[tunnelCode],fileName);
+  colonyTiles[tunnelCode] = document.createElement("img");
+  beginLoadingImage(colonyTiles[tunnelCode], fileName);
 }
 
 function loadImages() {
 
-  var imageList = [
-    {varName:groundBG, fileName:"Ground_Cross_Section.png"},
-    {varName:fungusNest, fileName:"Fungus_Nest.png"},
-    {varName:worker_right, fileName:"8bit-just-Worker-Right.png"},
-    
-    {tunnelType:COLONY_TUNNEL_VERT, fileName:"tunnel_tile_vert.png"},
-    {tunnelType:COLONY_TUNNEL_HOR, fileName:"tunnel_tile_hor.png"},
-    {tunnelType:COLONY_TUNNEL_4WAY, fileName:"dirt_tile.png"}
-    ];
+  var imageList = [{
+      varName: groundBG,
+      fileName: "Ground_Cross_Section.png"
+    },
+    {
+      varName: fungusNest,
+      fileName: "Fungus_Nest.png"
+    },
+    {
+      varName: worker_right,
+      fileName: "8bit-just-Worker-Right.png"
+    },
+
+    {
+      tunnelType: COLONY_TUNNEL_VERT,
+      fileName: "tunnel_tile_vert.png"
+    },
+    {
+      tunnelType: COLONY_TUNNEL_HOR,
+      fileName: "tunnel_tile_hor.png"
+    },
+    {
+      tunnelType: COLONY_TUNNEL_4WAY,
+      fileName: "dirt_tile.png"
+    }
+  ];
 
   picsToLoad = imageList.length;
 
-  for(var i=0;i<imageList.length;i++) {
-    if(imageList[i].tunnelType != undefined) {
+  for (var i = 0; i < imageList.length; i++) {
+    if (imageList[i].tunnelType != undefined) {
       loadImageForTrackCode(imageList[i].tunnelType, imageList[i].fileName);
     } else {
       beginLoadingImage(imageList[i].varName, imageList[i].fileName);
--- js/ui/flightGameUI.js
+++ js/ui/flightGameUI.js
@@ -1,4 +1,3 @@
-
 const FLIGHTGAMEUI = 1;
 var flightGameUI = [];
 
@@ -21,80 +20,77 @@ var camPanX = 0.0;
 const PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X = 10;
 
 
-function setupFlightGameUI()
-{
-    flyingQueen = new FlyingQueen(50, 250, true);
+function setupFlightGameUI() {
+  flyingQueen = new FlyingQueen(50, 250, true);
 
-    for (i = 0; i < flyingMaleCount; i++) {
-        flyingMales[i] = new FlyingMale();
-    }
-    for (i = 0; i < rivalQueenCount; i++) {
-      rivalQueens[i] = new FlyingQueen(Math.random() * gameWidth, Math.random() * gameHeight, false);
+  for (i = 0; i < flyingMaleCount; i++) {
+    flyingMales[i] = new FlyingMale();
+  }
+  for (i = 0; i < rivalQueenCount; i++) {
+    rivalQueens[i] = new FlyingQueen(Math.random() * gameWidth, Math.random() * gameHeight, false);
   }
 
 }
 
-function flightGameUICustomDraw(deltaTime)
-{
-   
-    totalSeconds += deltaTime;
-    var vx = 1;
-    var numImages = 2;
-    var xpos = totalSeconds * vx % gameWidth;
-   
-    renderer.save();
-    renderer.translate(-xpos, 0);
-    for (var i = 0; i < numImages; i++) {
-        renderer.drawImage(placeholder_BG, i * gameWidth, 0, gameWidth, gameHeight);
-    }
-    renderer.restore();
+function flightGameUICustomDraw(deltaTime) {
+
+  totalSeconds += deltaTime;
+  var vx = 1;
+  var numImages = 2;
+  var xpos = totalSeconds * vx % gameWidth;
+
+  renderer.save();
+  renderer.translate(-xpos, 0);
+  for (var i = 0; i < numImages; i++) {
+    renderer.drawImage(placeholder_BG, i * gameWidth, 0, gameWidth, gameHeight);
+  }
+  renderer.restore();
 
 
-    flyingQueen.draw(deltaTime);
+  flyingQueen.draw(deltaTime);
 
-    renderer.fillStyle = "white";
-    renderer.font = "34px Arial";
-    renderer.fillText("Mates: " + mateCount, 50, gameHeight - 50);
+  renderer.fillStyle = "white";
+  renderer.font = "34px Arial";
+  renderer.fillText("Mates: " + mateCount, 50, gameHeight - 50);
 
-    renderer.fillText("Genetic Diversity: ", gameWidth - 450, gameHeight - 100);
-    renderer.fillText("Energy: ", gameWidth - 300, gameHeight - 50);
+  renderer.fillText("Genetic Diversity: ", gameWidth - 450, gameHeight - 100);
+  renderer.fillText("Energy: ", gameWidth - 300, gameHeight - 50);
 
-    renderer.fillStyle = 'blue';
-    renderer.fillRect(gameWidth - 150, gameHeight - 125, diversityBarLength, 34);
-    renderer.beginPath();
-    renderer.strokeStyle = 'white';
-    renderer.rect(gameWidth - 150, gameHeight - 125, 100, 34);
-    renderer.stroke();        
+  renderer.fillStyle = 'blue';
+  renderer.fillRect(gameWidth - 150, gameHeight - 125, diversityBarLength, 34);
+  renderer.beginPath();
+  renderer.strokeStyle = 'white';
+  renderer.rect(gameWidth - 150, gameHeight - 125, 100, 34);
+  renderer.stroke();
 
-    renderer.fillStyle = 'green';
-    renderer.fillRect(gameWidth - 150, gameHeight - 75, energyBarLength, 34);
-    renderer.beginPath();
-    renderer.strokeStyle = 'white';
-    renderer.rect(gameWidth - 150, gameHeight - 75, 100, 34);
-    renderer.stroke();        
+  renderer.fillStyle = 'green';
+  renderer.fillRect(gameWidth - 150, gameHeight - 75, energyBarLength, 34);
+  renderer.beginPath();
+  renderer.strokeStyle = 'white';
+  renderer.rect(gameWidth - 150, gameHeight - 75, 100, 34);
+  renderer.stroke();
 }
 
-function flightGameUICustomEvents(deltaTime)
-{
-    if (flyingQueen.movementState == flyingQueen.movementStates.FLYING){
-        energyBarLength -= 0.15;
-    }
+function flightGameUICustomEvents(deltaTime) {
+  if (flyingQueen.movementState == flyingQueen.movementStates.FLYING) {
+    energyBarLength -= 0.15;
+  }
 
-    //cameraFollow();
-    
+  //cameraFollow();
+
 }
 
 function cameraFollow() {
-    var cameraFocusCenterX = camPanX + gameWidth/2;
+  var cameraFocusCenterX = camPanX + gameWidth / 2;
 
-    var playerDistFromCameraFocusX = Math.abs(flyingQueen.sprite.transform.position.x - cameraFocusCenterX);
+  var playerDistFromCameraFocusX = Math.abs(flyingQueen.sprite.transform.position.x - cameraFocusCenterX);
 
-    if(playerDistFromCameraFocusX > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X) {
-      if(cameraFocusCenterX < flyingQueen.sprite.transform.position.x)  {
-        camPanX += flyingQueen.horizontalSpeed;
-      } else {
-        camPanX -= flyingQueen.horizontalSpeed;
-      }
+  if (playerDistFromCameraFocusX > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X) {
+    if (cameraFocusCenterX < flyingQueen.sprite.transform.position.x) {
+      camPanX += flyingQueen.horizontalSpeed;
+    } else {
+      camPanX -= flyingQueen.horizontalSpeed;
     }
-
   }
+
+}
