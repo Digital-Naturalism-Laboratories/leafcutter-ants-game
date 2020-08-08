5428a87225eeeb7ee1fe8ce1b02b3db02b570582
stebssbets <stebssbets@yahoo.com>
2020-08-08 14:42:55 +0700

    switched initial UI state to main menu

--- js/ui/settings.js
+++ js/ui/settings.js
@@ -14,7 +14,7 @@ function uiInit()
     uistates.push(new UIState(colonyGameUI));
     uistates.push(new UIState(leafcuttingUI));
     uistates.push(new UIState(defenseGame.arrayOfUIObjects))
-    ui = new UI(uistates, COLONYGAMEUI);
+    ui = new UI(uistates, 0);
 }
 
 function uiSettings()
