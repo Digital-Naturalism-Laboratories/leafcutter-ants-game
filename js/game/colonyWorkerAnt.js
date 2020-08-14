d508f694525daff5652ee855c00d18d13001040c
brianjboucher <brianb823@gmail.com>
2020-08-14 07:20:55 -0500

    Colony Game - Added worker walking with Leaf animation state.

--- js/game/colonyWorkerAnt.js
+++ js/game/colonyWorkerAnt.js
@@ -1,16 +1,23 @@
+const MODES = {
+    CARRYING_LEAF: 'carrying_leaf',
+    NO_LEAF: 'no_leaf'
+}
+
 class ColonyWorkerAnt {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.horizontalSpeed = 2;
         this.verticalSpeed = 2;
-        this.sprite = new Sprite(tr(vec2(this.x * pixelSize, this.y * pixelSize), vec2(pixelSize * 0.4, pixelSize * 0.4)), new ImageObject("images/Animations/Worker_no_Leaf_Spritesheet.png", vec2(0, 0)));
+        this.sprite = new Sprite(tr(vec2(this.x * pixelSize, this.y * pixelSize), vec2(pixelSize * 0.4, pixelSize * 0.4)), new ImageObject("images/Animations/Worker_Walking_Spritesheet.png", vec2(0, 0)));
         this.collisionRadius = 15;
 
         this.animationFrameLength = 4;
         this.animationFrameCount = 11;
         this.animationFrameCurrent = 0;
         this.animationTimer = 0
+
+        this.mode = MODES.NO_LEAF;
 
         colonyGameUI.push(this);
     }
@@ -33,10 +40,14 @@ class ColonyWorkerAnt {
     }
 
     update() {
-        if (this.sprite.transform.position.x > gameWidth || this.sprite.transform.position.x < 0){
+        if (this.sprite.transform.position.x > gameWidth || this.sprite.transform.position.x < 0) {
             this.horizontalSpeed = -this.horizontalSpeed;
         }
         this.sprite.transform.position.x += this.horizontalSpeed;
+
+        this.x = this.sprite.transform.position.x;
+        this.y = this.sprite.transform.position.y;
+
     }
 
     draw() {
@@ -51,8 +62,19 @@ class ColonyWorkerAnt {
             y: 0
         }
 
+        switch (this.mode) {
+            case MODES.NO_LEAF:
+                inPos.y = inSize.y * 2;
+                break;
+            case MODES.CARRYING_LEAF:
+                break;
+        }
+
         if (this.horizontalSpeed > 0) {
             inPos.y = inSize.y;
+            this.mode = MODES.CARRYING_LEAF; //for testing only
+        } else {
+            this.mode = MODES.NO_LEAF; //For testing only
         }
 
         this.sprite.drawScIn(inPos, inSize);
