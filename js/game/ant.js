8129cb3fa9d5aa74b2b1b201068ad10fa824ad3d
bilalCh213 <47918386+bilalCh213@users.noreply.github.com>
2020-08-25 12:51:37 +0500

    Jaw controls: replaced red-green fade to instant color change.

--- js/game/ant.js
+++ js/game/ant.js
@@ -272,11 +272,13 @@ class Ant
             this.headLeadJawSprite.transform.position = tempPos;
             renderer.globalAlpha = 1;
             
+            var redColor = Math.floor(((this.timedJawRadius - this.timedJawMinRadius)*4.0)/200.0) * 200;
+            var greenColor = Math.floor(((this.timedJawMaxRadius - this.timedJawRadius)*4.0)/200.0) * 200;
+            if(redColor <= 0 && greenColor <= 0) { redColor = greenColor = 127 }
             drawCircle(renderer, this.isCuttingJawLed ? this.cuttingJawControlPos : this.leadingJawControlPos,
                 this.timedJawMinRadius, true, "#ffffff44", pixelSize);
             drawCircle(renderer, this.isCuttingJawLed ? this.cuttingJawControlPos : this.leadingJawControlPos,
-                this.timedJawRadius, false, this.jawSpeedPenalty <= 0 ? rgba(Math.floor((this.timedJawRadius - this.timedJawMinRadius)*4),
-                Math.floor((this.timedJawMaxRadius - this.timedJawRadius)*4), 0, 0.75) : "#99999955", (this.timedJawRadius - this.timedJawMinRadius) * pixelSize);
+                this.timedJawRadius, false, this.jawSpeedPenalty <= 0 ? rgba(redColor, greenColor, 0, 0.75) : "#99999955", (this.timedJawRadius - this.timedJawMinRadius) * pixelSize);
             renderer.lineWidth = 1;
         }
     }
