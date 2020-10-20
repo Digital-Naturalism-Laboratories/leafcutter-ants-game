12c81676f4f92bff7ba82c262d51c0def56fc873
stebssbets <stebssbets@yahoo.com>
2020-10-20 18:57:19 +0700

    put the flight game back as first game after main menu

--- js/ui/mainmenuUI.js
+++ js/ui/mainmenuUI.js
@@ -161,8 +161,8 @@ function mainMenuUICustomEvents(deltaTime)
 
     if(menuBigPlayButton.output == UIOUTPUT_SELECT)
     {
-        //ui.stateIndex = FLIGHTGAMEINTROUI;
-        ui.stateIndex = DEFENSEGAMEINTROUI;
+        ui.stateIndex = FLIGHTGAMEINTROUI;
+        //ui.stateIndex = DEFENSEGAMEINTROUI;
 
         //defenseGame.audioManager.startAmbience();
         menuBigPlayButton.resetOutput();
